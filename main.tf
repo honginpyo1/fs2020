9d7736edbdc506de4a4dc4e237e15eec607a62e2
inpyo hong <honginpyo1@hanmail.net>
2020-03-02 11:44:29 +0900

    sshkey

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "ibm_is_instance" "instance1" {
   }
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone1}"
-  keys = ["${ibm_is_ssh_key.ssh1.id}"]
+  keys = "${ibm_is_ssh_key.ssh1.id}"
   user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
 }
 
@@ -75,7 +75,7 @@ resource "ibm_is_instance" "instance2" {
   }
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone2}"
-  keys = ["${ibm_is_ssh_key.ssh1.id}"]
+  keys = "${ibm_is_ssh_key.ssh1.id}"
   user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
 }
 
@@ -89,7 +89,7 @@ resource "ibm_is_instance" "instance3" {
   }
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone3}"
-  keys = ["${ibm_is_ssh_key.ssh1.id}"]
+  keys = "${ibm_is_ssh_key.ssh1.id}"
   user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
 }
 
